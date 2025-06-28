a91febc24cdf432e45679fc123c97e1e311a5e17
Keva1z <Keva1z@yandex.ru>
2025-06-29 01:57:34 +0400

    feat: Add events parser

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # GitHub User Activity
+
+This is GitHub User activity tracker CLI
+
+***
+
+### How to use
+
+Clone repository and jump in it
+
+```bash
+git clone https://github.com/Keva1z/GitHub-Activity.git
+cd GitHub-Activity
+```
+
+Then install dependencies and run
+
+```bash
+pip install requests
+py github-activity.py Keva1z
+```
+
+***
+
+### Project source
+
+This project inspired from [Roadmap](https://www.roadmap.sh). Click this [link](https://roadmap.sh/projects/github-user-activity) to checkout the project
--- github-activity.py
+++ github-activity.py
@@ -1,4 +1,37 @@
-import sys
+import sys, requests
+
+def parse_events(events: list):
+    for event in events:
+        log: str = ''
+        if event['type'] == 'PushEvent':
+            log = f'Pushed {len(event['payload']['commits'])} commit(s) to {event['repo']['name']}'
+        elif event['type'] == 'IssuesEvent':
+            log = f'Opened a new issue in {event['repo']['name']}'
+        elif event['type'] == 'WatchEvent':
+            log = f'Starred {event['repo']['name']}'
+        elif event['type'] == 'ForkEvent':
+            log = f'Forked {event['repo']['name']}'
+        elif event['type'] == 'CreateEvent':
+            log = f'Created {event['payload']['ref_type']} in {event['repo']['name']}'
+        else:
+            log = f'{event['type'].replace('Event', '')} in {event['repo']['name']}'
+
+        print(f'- {log}')
+
+def make_request(username: str) -> list:
+    response = requests.get(f"https://api.github.com/users/{username}/events")
+
+    if not response.ok:
+        if response.status_code == 404:
+            raise Exception("User not found, please check username")
+        else:
+            raise Exception(f"Failed fetching data: {response.status_code}")
+    return response.json()
 
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) < 2: raise ValueError("No argument passed!")
+
+    events = make_request(sys.argv[1])
+    parse_events(events)
+
+
